f8c692c8e399e9b9219a10e7b175c6888aa677df
Timm Nawrocki <twnawrocki@alaska.edu>
2019-01-03 19:38:26 -0900

    Update for Wordpress 5
    Updated theme for compatibility with Wordpress 5 and associated Genesis Framework update. Most notably changed the menus, which now function from the conditional menus plugin rather than from global blox.

--- functions.php
+++ functions.php
@@ -8,190 +8,348 @@
  * @author  Timm Nawrocki
  * @license GPL-2.0+
  */
-// Start the engine.
-include_once( get_template_directory() . '/lib/init.php' );
-// Setup Theme.
-include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
-// Set Localization (do not remove).
-add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
-function genesis_sample_localization_setup(){
-	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
-}
-// Add the helper functions.
-include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
-// Add Image upload and Color select to WordPress Theme Customizer.
-require_once( get_stylesheet_directory() . '/lib/customize.php' );
-// Include Customizer CSS.
-include_once( get_stylesheet_directory() . '/lib/output.php' );
-// Add WooCommerce support.
-include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
-// Add the required WooCommerce styles and Customizer CSS.
-include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
-// Add the Genesis Connect WooCommerce notice.
-include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
-// Child theme (do not remove).
+
+// Starts the engine.
+require_once get_template_directory() . '/lib/init.php';
+
+// Defines the child theme (do not remove).
 define( 'CHILD_THEME_NAME', 'Wintermute' );
-define( 'CHILD_THEME_URL', 'https://accs.uaa.alaska.edu' );
-define( 'CHILD_THEME_VERSION', '1.0' );
-// Enqueue Scripts and Styles.
+define( 'CHILD_THEME_URL', 'https://github.com/accs-uaa' );
+define( 'CHILD_THEME_VERSION', '2.7.1' );
+
+// Sets up the Theme.
+require_once get_stylesheet_directory() . '/lib/theme-defaults.php';
+
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+/**
+ * Sets localization (do not remove).
+ *
+ * @since 1.0.0
+ */
+function genesis_sample_localization_setup() {
+
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
+
+}
+
+// Adds helper functions.
+require_once get_stylesheet_directory() . '/lib/helper-functions.php';
+
+// Adds image upload and color select to Customizer.
+require_once get_stylesheet_directory() . '/lib/customize.php';
+
+// Includes Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/output.php';
+
+// Adds WooCommerce support.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php';
+
+// Adds the required WooCommerce styles and Customizer CSS.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php';
+
+// Adds the Genesis Connect WooCommerce notice.
+require_once get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php';
+
+add_action( 'after_setup_theme', 'genesis_child_gutenberg_support' );
+/**
+ * Adds Gutenberg opt-in features and styling.
+ *
+ * @since 2.7.0
+ */
+function genesis_child_gutenberg_support() { // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- using same in all child themes to allow action to be unhooked.
+	require_once get_stylesheet_directory() . '/lib/gutenberg/init.php';
+}
+
 add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+/**
+ * Enqueues scripts and styles.
+ *
+ * @since 1.0.0
+ */
 function genesis_sample_enqueue_scripts_styles() {
-	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+
+	wp_enqueue_style(
+		'genesis-sample-fonts',
+		'//fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,700',
+		array(),
+		CHILD_THEME_VERSION
+	);
+
 	wp_enqueue_style( 'dashicons' );
+
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
-	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_enqueue_script(
+		'genesis-sample-responsive-menu',
+		get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js",
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+
 	wp_localize_script(
 		'genesis-sample-responsive-menu',
 		'genesis_responsive_menu',
 		genesis_sample_responsive_menu_settings()
 	);
+
+	wp_enqueue_script(
+		'genesis-sample',
+		get_stylesheet_directory_uri() . '/js/genesis-sample.js',
+		array( 'jquery' ),
+		CHILD_THEME_VERSION,
+		true
+	);
+
 }
-// Define our responsive menu settings.
+
+/**
+ * Defines responsive menu settings.
+ *
+ * @since 2.3.0
+ */
 function genesis_sample_responsive_menu_settings() {
+
 	$settings = array(
-		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
-		'menuIconClass'     => 'dashicons-before dashicons-menu',
-		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
-		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
-		'menuClasses'       => array(
+		'mainMenu'         => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'    => 'dashicons-before dashicons-menu',
+		'subMenu'          => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'      => array(
 			'combine' => array(
 				'.nav-primary',
-				'.nav-header',
 			),
 			'others'  => array(),
 		),
 	);
+
 	return $settings;
+
 }
-// Add HTML5 markup structure.
-add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
-// Add Accessibility support.
-add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
-// Add viewport meta tag for mobile browsers.
-add_theme_support( 'genesis-responsive-viewport' );
-// Add support for custom header.
-add_theme_support( 'custom-header', array(
-	'width'           => 875,
-	'height'          => 200,
-	'header-selector' => '.site-title a',
-	'header-text'     => false,
-	'flex-height'     => true,
-) );
-// Add support for custom background.
-add_theme_support( 'custom-background' );
-// Add support for after entry widget.
+
+// Adds support for HTML5 markup structure.
+add_theme_support(
+	'html5',
+	array(
+		'caption',
+		'comment-form',
+		'comment-list',
+		'gallery',
+		'search-form',
+	)
+);
+
+// Adds support for accessibility.
+add_theme_support(
+	'genesis-accessibility',
+	array(
+		'404-page',
+		'drop-down-menu',
+		'headings',
+		'search-form',
+		'skip-links',
+	)
+);
+
+// Adds viewport meta tag for mobile browsers.
+add_theme_support(
+	'genesis-responsive-viewport'
+);
+
+// Adds custom logo in Customizer > Site Identity.
+add_theme_support(
+	'custom-logo',
+	array(
+		'height'      => 120,
+		'width'       => 700,
+		'flex-height' => true,
+		'flex-width'  => true,
+	)
+);
+
+// Renames primary and secondary navigation menus.
+add_theme_support(
+	'genesis-menus',
+	array(
+		'primary'   => __( 'Header Menu', 'genesis-sample' ),
+		'secondary' => __( 'Footer Menu', 'genesis-sample' ),
+	)
+);
+
+// Adds image sizes.
+add_image_size( 'sidebar-featured', 75, 75, true );
+
+// Adds support for after entry widget.
 add_theme_support( 'genesis-after-entry-widget-area' );
-// Add support for 3-column footer widgets.
+
+// Adds support for 3-column footer widgets.
 add_theme_support( 'genesis-footer-widgets', 3 );
-// Add Image Sizes.
-add_image_size( 'featured-image', 720, 400, TRUE );
-// Rename primary and secondary navigation menus.
-add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
-// Reposition the secondary navigation menu.
+
+// Removes secondary sidebar.
+unregister_sidebar( 'sidebar-alt' );
+
+// Removes site layouts.
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
+
+// Removes output of primary navigation right extras.
+remove_filter( 'genesis_nav_items', 'genesis_nav_right', 10, 2 );
+remove_filter( 'wp_nav_menu_items', 'genesis_nav_right', 10, 2 );
+
+add_action( 'genesis_theme_settings_metaboxes', 'genesis_sample_remove_metaboxes' );
+/**
+ * Removes output of unused admin settings metaboxes.
+ *
+ * @since 2.6.0
+ *
+ * @param string $_genesis_admin_settings The admin screen to remove meta boxes from.
+ */
+function genesis_sample_remove_metaboxes( $_genesis_admin_settings ) {
+
+	remove_meta_box( 'genesis-theme-settings-header', $_genesis_admin_settings, 'main' );
+	remove_meta_box( 'genesis-theme-settings-nav', $_genesis_admin_settings, 'main' );
+
+}
+
+add_filter( 'genesis_customizer_theme_settings_config', 'genesis_sample_remove_customizer_settings' );
+/**
+ * Removes output of header settings in the Customizer.
+ *
+ * @since 2.6.0
+ *
+ * @param array $config Original Customizer items.
+ * @return array Filtered Customizer items.
+ */
+function genesis_sample_remove_customizer_settings( $config ) {
+
+	unset( $config['genesis']['sections']['genesis_header'] );
+	return $config;
+
+}
+
+// Displays custom logo.
+add_action( 'genesis_site_title', 'the_custom_logo', 0 );
+
+// Repositions primary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+add_action( 'genesis_header', 'genesis_do_nav', 12 );
+
+// Repositions the secondary navigation menu.
 remove_action( 'genesis_after_header', 'genesis_do_subnav' );
-add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
-// Reduce the secondary navigation menu to one level depth.
+add_action( 'genesis_footer', 'genesis_do_subnav', 10 );
+
 add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+/**
+ * Reduces secondary navigation menu to one level depth.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Original menu options.
+ * @return array Menu options with depth set to 1.
+ */
 function genesis_sample_secondary_menu_args( $args ) {
-	if ( 'secondary' != $args['theme_location'] ) {
+
+	if ( 'secondary' !== $args['theme_location'] ) {
 		return $args;
 	}
+
 	$args['depth'] = 1;
 	return $args;
+
 }
-// Modify size of the Gravatar in the author box.
+
 add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+/**
+ * Modifies size of the Gravatar in the author box.
+ *
+ * @since 2.2.3
+ *
+ * @param int $size Original icon size.
+ * @return int Modified icon size.
+ */
 function genesis_sample_author_box_gravatar( $size ) {
+
 	return 90;
+
 }
-// Modify size of the Gravatar in the entry comments.
+
 add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+/**
+ * Modifies size of the Gravatar in the entry comments.
+ *
+ * @since 2.2.3
+ *
+ * @param array $args Gravatar settings.
+ * @return array Gravatar settings with modified size.
+ */
 function genesis_sample_comments_gravatar( $args ) {
+
 	$args['avatar_size'] = 60;
 	return $args;
+
 }
+
 //* Functions added specific to Wintermute Theme
 //*----------------------------------------------------------------------------------------------
-function my_theme_setup() {
-	// Add support for wide images
-	add_theme_support( 'align-full' );
-}
-add_action( 'after_setup_theme', 'my_theme_setup' );
-// Change Favicon
+
+//* Change Favicon
 add_filter( 'genesis_pre_load_favicon', 'new_icon' );
 function new_icon( $icon) {
     $icon = '/favicon.ico';
     return $icon;
 }
-// Customize search form input box text
+
+//* Customize search form input box text
 add_filter( 'genesis_search_text', 'sp_search_text' );
 function sp_search_text( $text ) {
 	return esc_attr( 'Search...' );
 }
-// Add Dashicon to search form button 
+
+//* Add Dashicon to search form button 
 add_filter( 'genesis_search_button_text', 'wpsites_search_button_icon' );
 function wpsites_search_button_icon( $text ) {
 	return esc_attr( '&#xf179;' );
 }
-// Add custom footer
+
+//* Remove entry header from home page
+add_action( 'genesis_before', 'prefix_remove_entry_header' );
+function prefix_remove_entry_header()
+{
+
+	if ( ! is_front_page() ) { return; }
+
+	//* Remove the entry header markup (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
+	//* Remove the entry title (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+
+	//* Remove the entry meta in the entry header (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+
+	//* Remove the post format image (requires HTML5 theme support)
+	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
+}
+
+//* Add custom footer
 remove_action( 'genesis_footer', 'genesis_do_footer' );
 add_action( 'genesis_footer', 'sp_custom_footer' );
 function sp_custom_footer() {
 	?>
 	<div class="site-footer-inner">
-		<p>&copy; 2018 <a href="http://accs.uaa.alaska.edu">Alaska Center for Conservation Science</a>.This website was designed and is maintained by the University of Alaska Anchorage-<a href="https://accs.uaa.alaska.edu">Alaska Center for Conservation Science</a> (UAA-ACCS). The University of Alaska Anchorage is accredited by the Northwest Commission on Colleges and Universities. UA is an AA/EO employer and educational institution and prohibits illegal discrimination against any individual: <a title="UA non-discrimination policy" href="http://www.alaska.edu/nondiscrimination">UA Non-discrimination Policy</a>.</p>
+		<p>&copy; 2019 <a href="http://accs.uaa.alaska.edu">Alaska Center for Conservation Science</a>.This website was designed and is maintained by the University of Alaska Anchorage-<a href="https://accs.uaa.alaska.edu">Alaska Center for Conservation Science</a> (UAA-ACCS). The University of Alaska Anchorage is accredited by the Northwest Commission on Colleges and Universities. UA is an AA/EO employer and educational institution and prohibits illegal discrimination against any individual: <a title="UA non-discrimination policy" href="http://www.alaska.edu/nondiscrimination">UA Non-discrimination Policy</a>.</p>
 		<p><a href="mailto:twnawrocki@alaska.edu">Contact Website Administrator</a> | <a href="/sitemap/">Sitemap</a></p>
 	</div>
 	<?php
 }
-// Disable Visual Editor
-add_filter('user_can_richedit' , create_function('' , 'return false;') , 50);
-// Disable Add Media Button
-add_action('admin_init', 'remove_all_media_buttons');
-function remove_all_media_buttons() {
-	remove_all_actions('media_buttons');
-}
-// Disable Quicktags in HTML Editor
-add_filter('quicktags_settings', 'cyb_quicktags_settings');
-function cyb_quicktags_settings( $qtInit  ) {
-	//Set to emtpy string, empty array or false won't work. It must be set to ","
-	$qtInit['buttons'] = ',';
-	return $qtInit;
-}
-// Remove Admin Menus from Dashboard Sidebar
-function remove_menus() 
-	{
-	global $submenu;
-	remove_submenu_page ( 'themes.php', 'theme-editor.php' ); // Appearance-->Editor
-}
-add_action('admin_menu', 'remove_menus', 102);
-add_action( 'genesis_before', 'prefix_remove_entry_header' );
-// Remove admin bar for subscribers
-add_action('set_current_user', 'cc_hide_admin_bar');
-function cc_hide_admin_bar() {
-  if (!current_user_can('edit_posts')) {
-    show_admin_bar(false);
-  }
-}
-// Remove Entry Header
-function prefix_remove_entry_header()
-{
-	if ( ! is_front_page() ) { return; }
-	//* Remove the entry header markup (requires HTML5 theme support)
-	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
-	remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
-	//* Remove the entry title (requires HTML5 theme support)
-	remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
-	//* Remove the entry meta in the entry header (requires HTML5 theme support)
-	remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
-	//* Remove the post format image (requires HTML5 theme support)
-	remove_action( 'genesis_entry_header', 'genesis_do_post_format_image', 4 );
-}
+
 //* Enqueue Open Sans Google font
 add_action( 'wp_enqueue_scripts', 'sp_load_google_fonts' );
 function sp_load_google_fonts() {
 	wp_enqueue_style( 'google-font-opensans', '//fonts.googleapis.com/css?family=Open+Sans|Roboto+Condensed', array(), CHILD_THEME_VERSION );
 }
+
 //* Add mime types to for xml and zip to allow kml/kmz upload
 function add_upload_mimes($mimes) {
   $mimes['kml'] = 'application/xml';
@@ -199,3 +357,26 @@ function add_upload_mimes($mimes) {
   return $mimes;
 }
 add_filter('upload_mimes', 'add_upload_mimes');
+
+//* Disallow theme editor
+function disable_mytheme_action() {
+      define('DISALLOW_FILE_EDIT', TRUE);
+    }
+    add_action('init','disable_mytheme_action');
+	
+//* Remove Admin Menus from Dashboard Sidebar
+function remove_menus() 
+	{
+	global $submenu;
+	remove_submenu_page ( 'themes.php', 'theme-editor.php' ); // Appearance-->Editor
+}
+add_action('admin_menu', 'remove_menus', 102);
+add_action( 'genesis_before', 'prefix_remove_entry_header' );
+
+//* Remove admin bar for subscribers
+add_action('set_current_user', 'cc_hide_admin_bar');
+function cc_hide_admin_bar() {
+  if (!current_user_can('edit_posts')) {
+    show_admin_bar(false);
+  }
+}
--- lib/output.php
+++ lib/output.php
@@ -6,27 +6,38 @@
  *
  * @package Genesis Sample
  * @author  StudioPress
- * @license GPL-2.0+
- * @link    http://www.studiopress.com/
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
  */
 
 add_action( 'wp_enqueue_scripts', 'genesis_sample_css' );
 /**
-* Checks the settings for the link color, and accent color.
-* If any of these value are set the appropriate CSS is output.
-*
-* @since 2.2.3
-*/
+ * Checks the settings for the link color, and accent color.
+ * If any of these value are set the appropriate CSS is output.
+ *
+ * @since 2.2.3
+ */
 function genesis_sample_css() {
 
-	$handle  = defined( 'CHILD_THEME_NAME' ) && CHILD_THEME_NAME ? sanitize_title_with_dashes( CHILD_THEME_NAME ) : 'child-theme';
+	$handle = defined( 'CHILD_THEME_NAME' ) && CHILD_THEME_NAME ? sanitize_title_with_dashes( CHILD_THEME_NAME ) : 'child-theme';
 
-	$color_link = get_theme_mod( 'genesis_sample_link_color', genesis_sample_customizer_get_default_link_color() );
+	$color_link   = get_theme_mod( 'genesis_sample_link_color', genesis_sample_customizer_get_default_link_color() );
 	$color_accent = get_theme_mod( 'genesis_sample_accent_color', genesis_sample_customizer_get_default_accent_color() );
+	$logo         = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+
+	if ( $logo ) {
+		$logo_height           = absint( $logo[2] );
+		$logo_max_width        = get_theme_mod( 'genesis_sample_logo_width', 350 );
+		$logo_width            = absint( $logo[1] );
+		$logo_ratio            = $logo_width / max( $logo_height, 1 );
+		$logo_effective_height = min( $logo_width, $logo_max_width ) / max( $logo_ratio, 1 );
+		$logo_padding          = max( 0, ( 60 - $logo_effective_height ) / 2 );
+	}
 
 	$css = '';
 
-	$css .= ( genesis_sample_customizer_get_default_link_color() !== $color_link ) ? sprintf( '
+	$css .= ( genesis_sample_customizer_get_default_link_color() !== $color_link ) ? sprintf(
+		'
 
 		a,
 		.entry-title a:focus,
@@ -43,9 +54,12 @@ function genesis_sample_css() {
 			color: %s;
 		}
 
-		', $color_link ) : '';
+		',
+		$color_link
+	) : '';
 
-	$css .= ( genesis_sample_customizer_get_default_accent_color() !== $color_accent ) ? sprintf( '
+	$css .= ( genesis_sample_customizer_get_default_accent_color() !== $color_accent ) ? sprintf(
+		'
 
 		button:focus,
 		button:hover,
@@ -59,16 +73,78 @@ function genesis_sample_css() {
 		input[type="reset"]:hover,
 		input[type="submit"]:focus,
 		input[type="submit"]:hover,
-		.archive-pagination li a:focus,
-		.archive-pagination li a:hover,
-		.archive-pagination .active a,
 		.button:focus,
-		.button:hover,
-		.sidebar .enews-widget input[type="submit"] {
-			background-color: %s;
-			color: %s;
+		.button:hover {
+			background-color: %1$s;
+			color: %2$s;
 		}
-		', $color_accent, genesis_sample_color_contrast( $color_accent ) ) : '';
+
+		@media only screen and (min-width: 960px) {
+			.genesis-nav-menu > .menu-highlight > a:hover,
+			.genesis-nav-menu > .menu-highlight > a:focus,
+			.genesis-nav-menu > .menu-highlight.current-menu-item > a,
+			.content .wp-block-button .wp-block-button__link:focus,
+			.content .wp-block-button .wp-block-button__link:hover {
+				background-color: %1$s;
+				color: %2$s;
+			}
+		}
+		',
+		$color_accent,
+		genesis_sample_color_contrast( $color_accent )
+	) : '';
+
+	$css .= ( has_custom_logo() && ( 200 <= $logo_effective_height ) ) ?
+		'
+		.site-header {
+			position: static;
+		}
+		'
+	: '';
+
+	$css .= ( has_custom_logo() && ( 350 !== $logo_max_width ) ) ? sprintf(
+		'
+		.wp-custom-logo .site-container .title-area {
+			max-width: %spx;
+		}
+		',
+		$logo_max_width
+	) : '';
+
+	// Place menu below logo and center logo once it gets big.
+	$css .= ( has_custom_logo() && ( 600 <= $logo_max_width ) ) ?
+		'
+		.wp-custom-logo .title-area,
+		.wp-custom-logo .menu-toggle,
+		.wp-custom-logo .nav-primary {
+			float: none;
+		}
+
+		.wp-custom-logo .title-area {
+			margin: 0 auto;
+			text-align: center;
+		}
+
+		@media only screen and (min-width: 960px) {
+			.wp-custom-logo .nav-primary {
+				text-align: center;
+			}
+
+			.wp-custom-logo .nav-primary .sub-menu {
+				text-align: left;
+			}
+		}
+		'
+	: '';
+
+	$css .= ( has_custom_logo() && $logo_padding && ( 1 < $logo_effective_height ) ) ? sprintf(
+		'
+		.wp-custom-logo .title-area {
+			padding-top: %spx;
+		}
+		',
+		$logo_padding + 5
+	) : '';
 
 	if ( $css ) {
 		wp_add_inline_style( $handle, $css );
